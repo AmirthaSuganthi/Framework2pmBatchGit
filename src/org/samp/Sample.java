201d703fa34c92a405cb9862d210fe3dd8c9ac49
AmirthaSuganthi <amirthalakshmi1801@gmail.com>
2021-12-20 15:40:57 +0530

    B added her code

--- src/org/samp/Sample.java
+++ src/org/samp/Sample.java
@@ -5,5 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("Java selenium");
+		System.out.println("Python");
+		System.out.println("Manual testing");
 	}
 }
